feb2da8474091417ef4ed42ce2f12f04a27968b5
transloka786 <trnaativetransloka@gmail.com>
2025-04-13 03:16:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if target_aa:
 
     st.write(f"✅ Loaded {len(tRNAs)} tRNAs for {target_aa}")
     if len(tRNAs) > 0:
-        st.write(tRNAs[:2])
+        st.write(tRNAs:)
     else:
         st.warning("⚠️ No tRNAs loaded. Check FASTA file or matching logic.")
 
