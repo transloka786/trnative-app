6e92e6fe642db95af8283e6fc49b00d29b30ed89
transloka786 <trnaativetransloka@gmail.com>
2025-04-14 15:42:19 +0200

    Delete app.py

--- app.py
+++
@@ -1,3 +0,0 @@
-
-# tRNative app with mutation parsing, gene requirement, cis-penalty, and RMSF upload support
-print("✅ This is a placeholder for your complete app_mutation_cis_md_final.py. Ready to upload.")
